39612c2240bdda2c24ab63927ba66389a83a9c0d
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-07-19 13:14:57 +1000

    Added a functions project that connects to Azure signal R service. Created admin page which is a basic signal R chate Updated the Twitch page to show the signal R message for 2 seconds and then go blank

---
+++ Blazy/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazy.Models
+{
+    public class Message
+    {
+        public string sender { get; set; }
+        public string text { get; set; }
+    }
+}
