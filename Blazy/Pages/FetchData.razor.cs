60f7f0f16d9f69ba4e77488773019eab9a76a5b5
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-07-23 08:57:05 +1000

    Added Weather API function that gets data out of Azurre Table Storage Updated Fetch Data page to use new API

---
+++ Blazy/Pages/FetchData.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazyDomain.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blazy.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        public List<WeatherForecast> Forecasts;
+        [Inject] private HttpClient Http { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            //Forecasts = await Http.GetJsonAsync<List<WeatherForecast>>("sample-data/weather.json");
+            var url = "http://localhost:7071/api/weather";
+            Debug.WriteLine("Gettings data from {url}");
+            Forecasts = await Http.GetJsonAsync<List<WeatherForecast>>(url);
+
+        }
+    }
+}
---
+++ BlazyDomain/Models/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazyDomain.Models
+{
+    public class WeatherForecast : TableEntity
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
